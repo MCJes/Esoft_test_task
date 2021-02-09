80d376400e587e6c13168c1e406fc6ed7e1d08f1
MCJes <mcwiz@yandex.ru>
2021-02-09 12:59:09 +0500

    final commit fix

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
+// import cors from 'cors'
+import path from 'path'
 import userRouter from './routes/user.router.js'
 import tasksRouter from './routes/tasks.router.js'
 dotenv.config()
 const app = express()
 
-app.use(cors())
+const __dirname = path.resolve()
+
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //routes
 app.use('/api/user', userRouter)
 app.use('/api/tasks', tasksRouter)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
+
 
 app.listen(3000)
 
